8c7bc906e188fb4f0ee4e231c40b0d21f22630ee
Iven Krall <iven.krall@bewotec.de>
2020-02-20 13:41:33 +0100

    Added support for font extensions apart from the FontManager to allow for font overrides Moved PopupFactory creation to access modules for better JDK 9+ support

--- src/main/java/org/fernice/reflare/internal/impl/SunFontAccessorImpl.java
+++ src/main/java/org/fernice/reflare/internal/impl/SunFontAccessorImpl.java
@@ -34,46 +34,23 @@ public class SunFontAccessorImpl implements SunFontAccessor {
             "ultrabold", "ultra bold", "ultra-bold", //
     };
 
+    private static final int MERGING_STRATEGY_COMBINE = 0;
+    private static final int MERGING_STRATEGY_KEEP = 1;
+    private static final int MERGING_STRATEGY_OVERRIDE = 2;
+
     private static final AtomicBoolean initialized = new AtomicBoolean();
     private static final CountDownLatch initializationLatch = new CountDownLatch(1);
 
-    private static final Map<String, Set<Font>> fontFamilies = new HashMap<>();
+    private static volatile Map<String, Set<Font>> fontFamilies = new HashMap<>();
+
+    private static final Set<Font> fontExtensions = new HashSet<>();
+    private static final Set<Font> fontExtensionOverrides = new HashSet<>();
 
     @Nullable
     @Override
     public Font findFont(@NotNull String family, int weight, boolean italic) {
-        // Force the full initialization of the FontManager
-        if (!initialized.getAndSet(true)) {
-            new Font(family, Font.BOLD | Font.ITALIC, 12);
-
-            Font[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
-
-            for (Font font : fonts) {
-                String familyName = font.getFamily().toLowerCase();
-
-                Set<Font> fontFamily = fontFamilies.computeIfAbsent(familyName, (s) -> new HashSet<>());
-                fontFamily.add(font);
-
-                for (String weightName : WEIGHT_NAMES) {
-                    int index = familyName.indexOf(weightName);
-
-                    if (index >= 0) {
-                        String strippedFamilyName = familyName.substring(0, index).trim();
-
-                        Set<Font> additionalFontFamily = fontFamilies.computeIfAbsent(strippedFamilyName, (s) -> new HashSet<>());
-                        additionalFontFamily.add(font);
-                    }
-                }
-            }
-
-            initializationLatch.countDown();
-        } else {
-            try {
-                initializationLatch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                return null;
-            }
+        if (!ensureInitialized(family)) {
+            return null;
         }
 
         Set<Font> fontFamily = fontFamilies.get(family.toLowerCase());
@@ -103,5 +80,129 @@ public class SunFontAccessorImpl implements SunFontAccessor {
         } else {
             return null;
         }
+    }
+
+    private boolean ensureInitialized(@NotNull String family) {
+        // Force the full initialization of the FontManager
+        if (!initialized.getAndSet(true)) {
+            new Font(family, Font.BOLD | Font.ITALIC, 12);
+
+            refresh();
+
+            initializationLatch.countDown();
+        } else {
+            try {
+                initializationLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean registerFontExtension(@NotNull Font font, boolean override) {
+        if (override) {
+            fontExtensions.remove(font);
+            if (!fontExtensionOverrides.add(font)) {
+                return false;
+            }
+        } else {
+            fontExtensionOverrides.remove(font);
+            if (!fontExtensions.add(font)) {
+                return false;
+            }
+        }
+        return recomputeFontFamilies(font);
+    }
+
+    @Override
+    public boolean unregisterFontExtension(@NotNull Font font) {
+        boolean result = fontExtensions.remove(font);
+        result |= fontExtensionOverrides.remove(font);
+        return result;
+    }
+
+    @Override
+    public void refresh() {
+        recomputeFontFamilies(null);
+    }
+
+    private boolean recomputeFontFamilies(@Nullable Font target) {
+        boolean targetResult;
+
+        Map<String, Set<Font>> fontFamilies = new HashMap<>();
+
+        Font[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+        targetResult = mergeFontFamilies(fontFamilies, fonts, MERGING_STRATEGY_COMBINE, target);
+
+        Font[] fontExtensions = SunFontAccessorImpl.fontExtensions.toArray(new Font[0]);
+        targetResult |= mergeFontFamilies(fontFamilies, fontExtensions, MERGING_STRATEGY_KEEP, target);
+
+        Font[] fontExtensionOverrides = SunFontAccessorImpl.fontExtensionOverrides.toArray(new Font[0]);
+        targetResult |= mergeFontFamilies(fontFamilies, fontExtensionOverrides, MERGING_STRATEGY_OVERRIDE, target);
+
+        SunFontAccessorImpl.fontFamilies = fontFamilies;
+
+        return targetResult;
+    }
+
+    private boolean mergeFontFamilies(@NotNull Map<String, Set<Font>> fontFamilies, @NotNull Font[] fonts, int mergingStrategy, @Nullable Font target) {
+        boolean targetResult = false;
+
+        for (Font font : fonts) {
+            String familyName = font.getFamily().toLowerCase();
+
+            Set<Font> fontFamily = fontFamilies.computeIfAbsent(familyName, (s) -> new HashSet<>());
+            targetResult |= mergeAgentFont(fontFamily, font, mergingStrategy) && font == target;
+
+            for (String weightName : WEIGHT_NAMES) {
+                int index = familyName.indexOf(weightName);
+
+                if (index >= 0) {
+                    String strippedFamilyName = familyName.substring(0, index).trim();
+
+                    Set<Font> additionalFontFamily = fontFamilies.computeIfAbsent(strippedFamilyName, (s) -> new HashSet<>());
+                    targetResult |= mergeAgentFont(additionalFontFamily, font, mergingStrategy) && font == target;
+                }
+            }
+        }
+
+        return targetResult;
+    }
+
+    private boolean mergeAgentFont(@NotNull Set<Font> fonts, @NotNull Font font, int mergingStrategy) {
+        FontAccess fontAccess = FontAccess.getFontAccess();
+        Font2D font2D = fontAccess.getFont2D(font);
+
+        int fontWeight = font2D.getWeight();
+        boolean fontItalic = font2D.getStyle() == 2 || font2D.getStyle() == 3;
+
+        Font matchingFont = null;
+        for (Font candidateFont : fonts) {
+            Font2D candidateFont2D = fontAccess.getFont2D(candidateFont);
+
+            int candidateFontWeight = candidateFont2D.getWeight();
+            boolean candidateFontItalic = candidateFont2D.getStyle() == 2 || candidateFont2D.getStyle() == 3;
+
+            if (candidateFontWeight == fontWeight && candidateFontItalic == fontItalic) {
+                matchingFont = candidateFont;
+                break;
+            }
+        }
+
+        if (matchingFont != null) {
+            if (mergingStrategy == MERGING_STRATEGY_KEEP) {
+                return false;
+            }
+
+            if (mergingStrategy == MERGING_STRATEGY_OVERRIDE) {
+                fonts.remove(matchingFont);
+            }
+        }
+        fonts.add(font);
+
+        return true;
     }
 }
