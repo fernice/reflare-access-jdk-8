b04cf638ef1690a2f849ce03c91faadc91ac7f67
Iven Krall <iven.krall@bewotec.de>
2022-07-11 22:21:41 +0200

    Raised versions Update compability approach

---
+++ src/main/java/org/fernice/reflare/internal/impl/CompatibilityAccessorImpl.java
@@ -0,0 +1,10 @@
+package org.fernice.reflare.internal.impl;
+
+import org.fernice.reflare.internal.CompatibilityHelper.CompatibilityAccessor;
+
+public class CompatibilityAccessorImpl implements CompatibilityAccessor {
+
+    @Override
+    public void ensureCompatibility() {
+    }
+}
--- src/main/java/org/fernice/reflare/internal/impl/SwingUtilitiesAccessorImpl.java
+++ src/main/java/org/fernice/reflare/internal/impl/SwingUtilitiesAccessorImpl.java
@@ -9,8 +9,18 @@ import sun.swing.SwingUtilities2;
 public class SwingUtilitiesAccessorImpl implements SwingUtilitiesAccessor {
 
     @Override
+    public Object getBASICMENUITEMUI_MAX_TEXT_OFFSET() {
+        return SwingUtilities2.BASICMENUITEMUI_MAX_TEXT_OFFSET;
+    }
+
+    @Override
     public FontMetrics getFontMetrics(JComponent var0, Graphics var1) {
         return SwingUtilities2.getFontMetrics(var0, var1);
+    }
+
+    @Override
+    public void drawString(JComponent c, Graphics g, String text, int x, int y) {
+        SwingUtilities2.drawString(c, g, text, x, y);
     }
 
     @Override
